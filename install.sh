4a7c1dfd031b2239540bb51f12e8b7c3cd046b4a
RadioAddition <radioaddition@pm.me>
2024-11-16 22:50:46 -0600

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install --git https://github.com/RaphGL/Tuckr.git
 mv $HOME/.config $HOME/.config_bak
 mkdir $HOME/.config
-tuckr a \*
+tuckr set \*
