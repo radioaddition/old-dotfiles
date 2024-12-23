8cf3c2e1d144dc8da8b7f776c8d7412472c48376
radioaddition <radioaddition@pm.me>
2024-12-23 01:09:14 -0500

    modified:   Configs/Flatpak/.config/flatpaks 	modified:   Configs/brew/.config/Brewfile 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
 cargo install --git https://github.com/RaphGL/Tuckr.git
 mv $HOME/.config $HOME/.config_bak
 mkdir $HOME/.config
